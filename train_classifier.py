191f3567655aa823179d960eb58ea874da265618
avaxiao <ava_xiao@icloud.com>
2023-09-24 17:16:29 -0500

    update training

--- train_classifier.py
+++ train_classifier.py
@@ -7,7 +7,7 @@ import argparse
 import gc
 from sklearn.metrics import average_precision_score
 from sklearn.metrics import roc_auc_score
-def load_features(root_feature, root_label,save_root, split='training'):
+def load_features(root_feature, root_label,save_root, split='train'):
     feature_all = []
     label_all = []
     weight_all = []
@@ -16,7 +16,7 @@ def load_features(root_feature, root_label,save_root, split='training'):
     label_files = os.listdir(label_root)
 
     for file_name in tqdm(label_files):
-        with open(os.path.join(root_feature, file_name), 'rb') as f:
+        with open(os.path.join(root_feature, split, file_name), 'rb') as f:
             file_features = pickle.load(f)
 
         with open(os.path.join(label_root, file_name), 'rb') as f:
@@ -43,14 +43,14 @@ def load_features(root_feature, root_label,save_root, split='training'):
 
 
 def evaluate_classifier(root_feature, root_label, save_root,class_id):
-    file = os.path.join(save_root, 'validation.pkl')
+    file = os.path.join(save_root, 'val.pkl')
     if os.path.exists(file):
         with open(file, 'rb') as f:
             print('Loading saved val data')
             val_data = pickle.load(f)
     else:
         print('Combining val features')
-        val_data = load_features(root_feature, root_label, save_root,'validation')
+        val_data = load_features(root_feature, root_label, save_root,'val')
 
  
     # load the model from disk
@@ -69,14 +69,14 @@ def evaluate_classifier(root_feature, root_label, save_root,class_id):
 
 
 def train_classifier(root_feature, root_label, save_root, class_id):
-    file = os.path.join(save_root, 'training.pkl')
+    file = os.path.join(save_root, 'train.pkl')
     if os.path.exists(file):
         with open(file, 'rb') as f:
             print('Loading saved features')
             train_data = pickle.load(f)
     else:
         print('Combining training features')
-        train_data = load_features(root_feature, root_label,save_root, 'training')
+        train_data = load_features(root_feature, root_label, save_root, 'train')
 
     print(f'Learning features for {class_id}')
     target_label = np.zeros_like(train_data['label'])
@@ -95,9 +95,10 @@ def train_classifier(root_feature, root_label, save_root, class_id):
 
 
 if __name__ == '__main__':
-    root_feature = '/shared/rsaas/dino_sam/pooled_features_pkl'
-    root_label = '/shared/rsaas/dino_sam/labels/ADE20K'
-    save_root = '/shared/rsaas/dino_sam'
+    root_feature = '/shared/rsaas/dino_sam/pooled_features_pkl/pascal_voc'
+    root_label = '/shared/rsaas/dino_sam/labels/pascal_voc'
+    save_root = '/shared/rsaas/dino_sam/classifiers/pascal_voc'
+    os.makedirs(save_root, exist_ok=True)
 
 
     parser = argparse.ArgumentParser()
@@ -107,12 +108,12 @@ if __name__ == '__main__':
     parser.add_argument('--min_class_id',default=-1)
     parser.add_argument('--include_0_class',action='store_true')
     args = parser.parse_args()
-    training_file = os.path.join(save_root, 'training.pkl')
-    val_file = os.path.join(save_root,'validation.pkl')
-    if not os.path.exists(training_file):
-        train_data = load_features(root_feature, root_label,save_root, 'training')
+    train_file = os.path.join(save_root, 'train.pkl')
+    val_file = os.path.join(save_root,'val.pkl')
+    if not os.path.exists(train_file):
+        train_data = load_features(root_feature, root_label,save_root, 'train')
     if not os.path.exists(val_file):
-        val_data = load_features(root_feature, root_label,save_root, 'validation')
+        val_data = load_features(root_feature, root_label,save_root, 'val')
 
     
     if args.max_class_id == -1 or args.min_class_id == -1:
@@ -122,7 +123,7 @@ if __name__ == '__main__':
 
         if class_ids[0] == 0 and args.include_0_class == False:
             # we do not want to train the 0th class 
-            min_class  = class_ids[1]
+            min_class = class_ids[1]
     else:
         min_class = args.min_class_id
         max_class = args.max_class_id
