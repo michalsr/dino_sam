84fb991bd409b8f0cd8d93a99d45ea10b6ea4809
Michal Shlapentokh-Rothman <michalsr25@gmail.com>
2023-09-25 09:17:31 -0500

    fix label stuff

--- label_and_visualize.py
+++ label_and_visualize.py
@@ -4,16 +4,19 @@ from PIL import Image, ImageColor
 from pycocotools import mask as mask_utils
 from tqdm import tqdm
 import os 
+import scipy
 import cv2
 import pickle 
+from color_maps.pascal_voc import VOC2012_COLORMAP
 class LabelRegions(object):
-     def __init__(self,annotation_location,sam_location,label_directory,num_labels=150,device='cuda'):
+     def __init__(self,annotation_location,sam_location,label_directory,color_map,num_labels=150,device='cuda'):
         self.annotation_location = annotation_location
         self.sam_location = sam_location 
         self.label_directory = label_directory
         self.device = device 
         self.image_id_to_sam = None 
         self.num_labels = num_labels
+        self.color_map = color_map
      def load_all_sam_regions(self):
         image_id_to_sam = {}
         for f in tqdm(os.listdir(self.sam_location)):
@@ -22,6 +25,29 @@ class LabelRegions(object):
             image_id_to_sam[f.replace('.json','')] = sam_regions
         self.image_id_to_sam = image_id_to_sam
         return image_id_to_sam
+     def label_region_pascal(self,sam_region,annotation_map):
+        sam_region_nonzero = np.where(sam_region != 0)
+        rgb_vals = [annotation_map[x,y,:] for x,y in zip(sam_region_nonzero[0],sam_region_nonzero[1])]
+        # get pixel values from map
+        classes = [np.where(np.all(self.color_map == rgb_vals[-1],axis=-1))[0] for a in rgb_vals] 
+        all_classes = [c[0] for c in classes if len(c)!=0]
+        unique_pixels, pixel_counts = np.unique(all_classes,return_counts=True)
+        all_pixels_in_region = dict(zip(unique_pixels,pixel_counts))
+        more_than_95 = [pixel_val for pixel_val,pixel_count in all_pixels_in_region.items()]
+        initial_label  = {key: None for key in list(range(1,self.num_labels+1))}
+        final_label = {}
+        if len(more_than_95)>0 and more_than_95[0]>0:
+            # positive for that label 
+            assert len(more_than_95)<2 
+            final_label[more_than_95[0]] = 1
+            # negative for the rest 
+            for key in list(range(1,self.num_labels+1)):
+                if key != more_than_95[0]:
+                    final_label[key] = -1 
+        else:
+            # all zero 
+            final_label = {key:0 for key in list(range(1,self.num_labels+1))}     
+        return final_label
      def label_region(self,sam_region,annotation_map):
 
         assert sam_region.shape[0] == annotation_map.shape[0]
@@ -52,12 +78,30 @@ class LabelRegions(object):
             # all zero 
             final_label = {key:0 for key in list(range(1,self.num_labels+1))}      
         return final_label
+
      def save_region_labels(self,image_label_dict,file_name):
         if not os.path.exists(self.label_directory):
             os.makedirs(self.label_directory)
         with open(os.path.join(self.label_directory,file_name),'wb+') as f:
                 pickle.dump(image_label_dict,f)
 
+     def convert_annotation_map(self,annotation_map):
+        h, w= annotation_map.shape
+        num_classes = 21
+
+        r = annotation_map.copy()
+        g = annotation_map.copy()
+        b = annotation_map.copy()
+        for ll in range(0, 21):
+            r[annotation_map == ll] = self.color_map[ll, 0]
+            g[annotation_map== ll] = self.color_map[ll, 1]
+            b[annotation_map == ll] = self.color_map[ll, 2]
+        rgb = np.zeros((annotation_map.shape[0], annotation_map.shape[1], 3))
+        rgb[:, :, 0] = r 
+        rgb[:, :, 1] = g 
+        rgb[:, :, 2] = b 
+                
+        return rgb
 
 
      def label_all_regions_all_images(self):
@@ -65,16 +109,21 @@ class LabelRegions(object):
         all_annotations = os.listdir(self.annotation_location)
         for i,ann in enumerate(tqdm(all_annotations,desc='Label Features',total=len(all_annotations))):
             region_to_label = []
-
-            annotation_map = cv2.imread(os.path.join(self.annotation_location,ann))
+            self.color_map = np.array(self.color_map)
+            annotation_map =np.array(Image.open(os.path.join(self.annotation_location,ann)))
+  
+            annotation_map = self.convert_annotation_map(annotation_map)
+        
+     
             sam_regions = self.image_id_to_sam[ann.replace('.png','')]
             for region in sam_regions:
                 sam_labels = {}
                 sam_labels['region_id'] = region['region_id']
                 sam_mask = mask_utils.decode(region['segmentation'])
-                labels  = self.label_region(sam_mask,annotation_map)
+                labels  = self.label_region_pascal(sam_mask,annotation_map)
                 sam_labels['labels'] = labels 
                 region_to_label.append(sam_labels)
+
             self.save_region_labels(region_to_label,ann.replace('.png','.pkl'))
         def map_on_img(self,image_name,mask,color='blue',alpha=0.8):
             img = cv2.imread(os.path.join(self.image_location,name))
@@ -91,7 +140,7 @@ class LabelRegions(object):
                 arr_1, arr_2, _ = np.where(first_map == label)
                 color_seg[arr_1,arr_2,:] = np.array(color)
             return color_seg 
-labels = LabelRegions(annotation_location='/shared/rsaas/dino_sam/data/VOCdevkit/VOC2012/segmentation_annotation/val',sam_location='/home/michal5/dino_sam/sam_pascal_voc/val',label_directory='/shared/rsaas/dino_sam/labels/pascal_voc/val')
+labels = LabelRegions(annotation_location='/shared/rsaas/dino_sam/data/VOCdevkit/VOC2012/segmentation_annotation/train',sam_location='/home/michal5/dino_sam/sam_pascal_voc/train',label_directory='/shared/rsaas/dino_sam/labels/pascal_voc/train',color_map=VOC2012_COLORMAP,num_labels=20)
 labels.load_all_sam_regions()
 labels.label_all_regions_all_images()
 
--- train_classifier.py
+++ train_classifier.py
@@ -14,9 +14,11 @@ def load_features(root_feature, root_label,save_root, split='train'):
 
     label_root = os.path.join(root_label, split)
     label_files = os.listdir(label_root)
+    feature_root = os.path.join(root_feature,split)
+
 
     for file_name in tqdm(label_files):
-        with open(os.path.join(root_feature, split, file_name), 'rb') as f:
+        with open(os.path.join(feature_root, file_name), 'rb') as f:
             file_features = pickle.load(f)
 
         with open(os.path.join(label_root, file_name), 'rb') as f:
@@ -25,18 +27,24 @@ def load_features(root_feature, root_label,save_root, split='train'):
         for i, area in enumerate(file_features):
             area_feature = area['pooled_region']
             area_label = file_labels[i]['labels']
+  
             target_label = list(area_label.keys())[0]
-
+  
             if area_label[target_label] == 1:
+                assert target_label<21
+
+
                 feature_all.append(area_feature)
                 label_all.append(target_label)
                 weight_all.append(area['area'])
 
     save_dict = {}
+
     save_dict['feature'] = np.stack(feature_all)
     save_dict['label'] = np.stack(label_all)
     save_dict['weight'] = np.stack(weight_all)
-
+    if not os.path.exists(save_root):
+        os.makedirs(save_root)
     with open(os.path.join(save_root, split + '.pkl'), 'wb') as f:
         pickle.dump(save_dict, f)
     return save_dict
@@ -61,9 +69,9 @@ def evaluate_classifier(root_feature, root_label, save_root,class_id):
     target_label = np.zeros_like(val_data['label'])
     target_label.fill(-1)
     target_label[np.where(val_data['label'] == class_id)] = 1
-    roc_auc_score = roc_auc_score(target_label,loaded_model.decision_function(val_data['feature']),sample_weight=val_data['weight'], multi_class='ovr')
+    roc_auc = roc_auc_score(target_label,loaded_model.decision_function(val_data['feature']),sample_weight=val_data['weight'], multi_class='ovr')
     ap_score = average_precision_score(target_label,loaded_model.decision_function(val_data['feature']),sample_weight=val_data['weight'])
-    print(f'ROC AUC SCORE = {roc_auc_score} for class {class_id}')
+    print(f'ROC AUC SCORE = {roc_auc} for class {class_id}')
     print(f'Average Precision Score = {ap_score} for class {class_id}')
     del loaded_model
 
@@ -76,7 +84,7 @@ def train_classifier(root_feature, root_label, save_root, class_id):
             train_data = pickle.load(f)
     else:
         print('Combining training features')
-        train_data = load_features(root_feature, root_label, save_root, 'train')
+        train_data = load_features(root_feature, root_label,save_root, 'train')
 
     print(f'Learning features for {class_id}')
     target_label = np.zeros_like(train_data['label'])
@@ -87,6 +95,8 @@ def train_classifier(root_feature, root_label, save_root, class_id):
                                                                                     target_label,
                                                                                     sample_weight=train_data['weight'])
     # save the model to disk
+    if not os.path.exists(save_root):
+        os.makedirs(save_root)
     filename = f'{save_root}/model_{class_id}.sav'
     with open(filename,'wb') as s:
         pickle.dump(classifier, s)
@@ -97,8 +107,12 @@ def train_classifier(root_feature, root_label, save_root, class_id):
 if __name__ == '__main__':
     root_feature = '/shared/rsaas/dino_sam/pooled_features_pkl/pascal_voc'
     root_label = '/shared/rsaas/dino_sam/labels/pascal_voc'
+<<<<<<< HEAD
     save_root = '/shared/rsaas/dino_sam/classifiers/pascal_voc'
     os.makedirs(save_root, exist_ok=True)
+=======
+    save_root = '/home/michal5/dino_sam/classifiers/pascal_voc'
+>>>>>>> c35db86 (fix label stuff)
 
 
     parser = argparse.ArgumentParser()
@@ -108,16 +122,23 @@ if __name__ == '__main__':
     parser.add_argument('--min_class_id',default=-1)
     parser.add_argument('--include_0_class',action='store_true')
     args = parser.parse_args()
-    train_file = os.path.join(save_root, 'train.pkl')
+    training_file = os.path.join(save_root, 'train.pkl')
     val_file = os.path.join(save_root,'val.pkl')
-    if not os.path.exists(train_file):
+    if not os.path.exists(training_file):
         train_data = load_features(root_feature, root_label,save_root, 'train')
+    else:
+        with open(training_file,'rb') as t:
+            train_data = pickle.load(t)
     if not os.path.exists(val_file):
         val_data = load_features(root_feature, root_label,save_root, 'val')
+    else:
+        with open(val_file,'rb') as v:
+            val_data = pickle.load(v)
 
     
     if args.max_class_id == -1 or args.min_class_id == -1:
-        class_ids = np.unique(train_data['labels'])
+        class_ids = np.unique(train_data['label'])
+        print(class_ids, 'class ids')
         min_class = class_ids[0]
         max_class= class_ids[-1]
 
