96983fcda164d455da7a13f5870921ee67c9b457
Michal Shlapentokh-Rothman <michalsr25@gmail.com>
2023-11-14 18:19:41 -0600

    label regions

--- label_regions.py
+++ label_regions.py
@@ -31,12 +31,11 @@ def label_region(args,sam_region,annotation_map):
     unique_pixels, pixel_counts = np.unique(pixel_values_in_region,return_counts=True)
     all_pixels_in_region = dict(zip(unique_pixels,pixel_counts))
     start_class = 0
-    if args.ignore_zero:
-        start_class = 1
+
     # get total num of pixels
     num_pixels = sum(all_pixels_in_region.values())
     #check if any pixel is greater than certain percent value
-    more_than_percent= [(pixel_val,pixel_count) for pixel_val,pixel_count in all_pixels_in_region.items() if all((pixel_count>((args.label_percent/100)*num_pixels),pixel_val>=start_class,pixel_val<=args.num_classes+1))]
+    more_than_percent= [(pixel_val,pixel_count) for pixel_val,pixel_count in all_pixels_in_region.items() if all((pixel_count>((args.label_percent/100)*num_pixels),pixel_val>=0,pixel_val<=args.num_classes))]
     # initialize all as None
 
     initial_label  = {key: None for key in list(range(start_class,args.num_classes+1))}
@@ -50,53 +49,13 @@ def label_region(args,sam_region,annotation_map):
 
         final_label[max_pixel_class] = 1
         # negative for the rest
-        for key in list(range(start_class,int(args.num_classes)+1)):
+        for key in list(range(start_class,int(args.num_classes))):
             if key != max_pixel_class:
                 final_label[key] = -1
     else:
         # all zero
-        final_label = {key:0 for key in list(range(start_class,int(args.num_classes)+1))}
+        final_label = {key:0 for key in list(range(start_class,int(args.num_classes)))}
     return final_label
-def label_vaw(args):
-    # Already have regions from data
-    # each entry has image id, polygon coordinates
-    annotation_file = utils.open_file(os.path.join(args.annotation_dir,args.annotation_file))
-
-    # annotation file uses actual class names not numbers
-    label_map = utils.open_file(args.label_map)
-    # map images to instances to save space
-    all_image_ids = [entry['image_id'] for entry in annotation_file]
-
-    image_to_instances = {key:[] for key in all_image_ids}
-
-    for entry in tqdm(annotation_file):
-        instance_dict = {}
-        instance_dict['instance_id'] = entry['instance_id']
-
-         # 620 attributes
-        instance_labels  = {key: -1 for key in list(range(0,621))}
-        if not args.use_sam:
-            # convert polygon to region
-            # file name is instance id
-            img = np.array(Image.open(os.path.join(args.image_dir,entry['image_id']+'.jpg')))
-
-            h, w = img.shape[0],img.shape[1]
-            if entry['instance_polygon'] != None:
-                mask = utils.polygon_to_mask(entry['instance_polygon'],h,w)
-                instance_dict['mask'] = mask
-            else:
-                continue
-
-            # just read attributes from file
-            for p in entry['positive_attributes']:
-                idx = label_map[p]
-                instance_labels[idx] = 1
-        instance_dict['labels'] = instance_labels
-        image_to_instances[entry['image_id']].append(instance_dict)
-    for image_id in tqdm(image_to_instances):
-        utils.save_file(os.path.join(args.region_labels,image_id+'.pkl'),image_to_instances[image_id])
-
-
 
 
 
@@ -175,11 +134,6 @@ if __name__ == '__main__':
         help="Only for VAW. Whether to use SAM regions or default instance masks",
     )
     parser.add_argument(
-        "--ignore_zero",
-        action="store_true",
-        help="Include 0 class"
-    )
-    parser.add_argument(
         "--num_classes",
         type=int,
         default=0,
@@ -204,8 +158,6 @@ if __name__ == '__main__':
         help="Image dir for vaw if needed for saving masks")
 
     args = parser.parse_args()
-    if args.vaw:
-        print('Labeling VAW dataset')
-        label_vaw(args)
-    else:
-        label_all_regions(args)
+    if args.num_classes != 151 and args.num_classes!= 21:
+        raise ValueError('ADE should have 151 and Pascal VOC should have 21')
+    label_all_regions(args)
